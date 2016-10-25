a2916b45ee37bfd00aa597086e79fe2532bc27ef
Ignacio Marquez <jgmarquez@uc.cl>
2016-10-24 23:27:02 -0300

    fix compras id

--- app/models/compra.rb
+++ app/models/compra.rb
@@ -2,7 +2,7 @@ class Compra < ActiveRecord::Base
     after_create :actualizar_stock
 
     def actualizar_stock
-        item = Item.find_by_identificador(self.producto)
+        item = Item.find(self.producto)
         item.actualizar_stock(self.cantidad.to_i)
     end
     
